13adc83368384ad2375d2370e7c34194f4443099
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-11-13 10:52:11 +0000

    feat: init

--- app.js
+++ app.js
@@ -10,34 +10,34 @@ app.use(
   })
 )
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 app.get('/', (req, res) => {
   res.send({ hello: 'world' })
 })
 
-app.post('/create-payment-intent', async (req, res) => {
-  let { payment_method_types = ['card'], currency = 'eur', amount = 1000 } = req.body
-  if (!amount || amount < 500) {
-    amount = 500
-  }
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: 'eur',
-      payment_method_types,
-      currency
-    })
-    res.json({
-      clientSecret: paymentIntent.client_secret
-    })
-  } catch (e) {
-    res.status(400).json({
-      error: {
-        message: e.message
-      }
-    })
-  }
-})
+// app.post('/create-payment-intent', async (req, res) => {
+//   let { payment_method_types = ['card'], currency = 'eur', amount = 1000 } = req.body
+//   if (!amount || amount < 500) {
+//     amount = 500
+//   }
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount,
+//       currency: 'eur',
+//       payment_method_types,
+//       currency
+//     })
+//     res.json({
+//       clientSecret: paymentIntent.client_secret
+//     })
+//   } catch (e) {
+//     res.status(400).json({
+//       error: {
+//         message: e.message
+//       }
+//     })
+//   }
+// })
 
 app.listen(3000)
