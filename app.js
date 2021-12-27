ae63e352b0e779b3a6c4715255906741832577fd
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-12-27 14:34:27 +0000

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,121 @@
 require("dotenv").config()
-
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
+const fs = require('fs');
+const cors = require('cors')
+const axios = require('axios')
+const schedule = require('node-schedule');
+
+const PORT = process.env.PORT || 4000
+let redirect_uri = 'http://localhost:4000/callback'
+let zoomUrl = 'https://api.zoom.us/v2/users/'
+
+const headers = {
+  Authorization: 'Basic ' + Buffer.from(process.env.ZOOM_CLIENT_ID + ':' + process.env.ZOOM_SECRET).toString('base64'),
+  'Content-type': "application/x-www-form-urlencoded"
+}
+
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Routes
+ */
 
 app.get('/', function (req, res) {
-  res.json({
-    "model": "world"
-  })
+  let rawdata = fs.readFileSync('tokens.json');
+  let tokensSaved = JSON.parse(rawdata);
+  res.json(tokensSaved)
 })
 
-app.listen(PORT)
+app.get('/callback', async (req, res) => {
+  await askToken(req.query.code);
+  await res.json(200)
+})
 
-// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
-// app.post('/create-payment-intent', async (req, res) => {
-//   let { payment_method_types = ['card'], currency = 'eur', amount = 1000 } = req.body
-//   if (!amount || amount < 500) {
-//     amount = 500
-//   }
-//   try {
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount,
-//       currency: 'eur',
-//       payment_method_types,
-//       currency
-//     })
-//     res.json({
-//       clientSecret: paymentIntent.client_secret
-//     })
-//   } catch (e) {
-//     res.status(400).json({
-//       error: {
-//         message: e.message
-//       }
-//     })
-//   }
-// })
+app.post('/createMeeting', async (req, res) => {
+  let rawdata = await fs.readFileSync('tokens.json');
+  let tokensSaved = JSON.parse(rawdata);
+
+  await axios.post(`${zoomUrl}me/meetings`, req.body, {
+    headers: {
+      Authorization: 'Bearer ' + tokensSaved.access_token
+    }
+  })
+    .then((response) => {
+      console.log("🟢 Meeting created!");
+      res.send({
+        status: 200,
+        message: 'Meeting successfully created.',
+        meeting: response.data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send({
+        status: 400,
+        message: 'Error while creating meeting.'
+      })
+    })
+})
+
+/**
+ * Functions
+ */
+
+const writeTokens = (res) => {
+  const { access_token, refresh_token } = res
+  let tokens = {}
+  tokens.access_token = access_token
+  tokens.refresh_token = refresh_token
+  let data = JSON.stringify(tokens);
+
+  fs.writeFile('tokens.json', data, (err) => {
+    if (err) throw err;
+    console.log("🟢 Token updated!");
+  })
+}
+
+const askToken = async (code) => {
+  await axios.post('https://zoom.us/oauth/token', {}, {
+    headers,
+    params: {
+      grant_type: "authorization_code",
+      code,
+      redirect_uri,
+    }
+  })
+    .then((response) => {
+      writeTokens(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+const refresh = async () => {
+  let rawdata = fs.readFileSync('tokens.json');
+  let tokensSaved = JSON.parse(rawdata);
+  await axios.post('https://zoom.us/oauth/token', {}, {
+    headers,
+    params: {
+      grant_type: "refresh_token",
+      refresh_token: tokensSaved.refresh_token
+    }
+  })
+    .then((response) => {
+      writeTokens(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+const job = schedule.scheduleJob('0 1 * * * *', function () {
+  /**
+   * Refresh our token every hour
+   */
+  refresh()
+});
+
+app.listen(PORT)
